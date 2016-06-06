12f95e2d991d87f679ad2953240f05e28d2c9489
Alexandre SCHANNE <alexandre.schanne@telecomanancy.eu>
2016-06-06 10:37:28 +0200

    Sommet

---
+++ Sommet.java
@@ -0,0 +1,19 @@
+package senCity;
+
+public class Sommet {
+	
+	String numero;
+	String numero2;
+
+	public Sommet(String numero){
+		this.numero=numero;
+	}
+	
+	public String toString(){
+		return numero;
+	}
+	
+	public String getSommet(){
+		return this.numero;
+	}
+}
